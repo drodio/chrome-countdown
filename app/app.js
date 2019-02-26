4d7bf92695afe3ac3c50f68ec00788f3d1595319
away168 <andrew@wayfamily.net>
2019-02-26 14:19:16 -0800

    Change date display format to Days, Hours, Minutes, Seconds

--- app/app.js
+++ app/app.js
@@ -13,20 +13,28 @@ var App = function($el){
 App.fn = App.prototype
 
 App.fn.load = function(){
-  this.end = new Date(parseInt(1593527101193))
+  this.end = new Date("01/15/2020")
 }
 
 App.fn.renderTimeLoop = function(){
-  this.interval = setInterval(this.renderTime.bind(this), 100)
+  this.interval = setInterval(this.renderTime.bind(this), 1000)
 }
 
 
 function getDayDiff (first, second) {
-  var diff = (second-first)/(1000*60*60*24)
+  var distance = (second-first)  // in milliseconds /(1000*60*60*24) // 1000ms/s * 60s/m * 60m/hr * 24 hours/day == diff day/ms
+
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   return {
-    days: Math.floor(diff),
-    hours: (diff - Math.floor(diff))
+    days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+    hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+    seconds: Math.floor((distance % (1000 * 60)) / 1000)
   }
 }
 
@@ -38,7 +46,9 @@ App.fn.renderTime = function(){
   requestAnimationFrame(function(){
     this.html(this.view('age')({
       days: diff.days,
-      hours: Math.floor(diff.hours * 10000000)
+      hours: diff.hours,
+      mins: diff.minutes,
+      secs: diff.seconds
     }))
   }.bind(this))
 }

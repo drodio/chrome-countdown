b12e15864926e08487e1ced95afaa42a0676a979
away168 <andrew@wayfamily.net>
2019-02-28 11:04:47 -0800

    pad hours, minutes, seconds with leading 0s

--- app/app.js
+++ app/app.js
@@ -32,9 +32,9 @@ function getDayDiff (first, second) {
 
   return {
     days: Math.floor(distance / (1000 * 60 * 60 * 24)),
-    hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-    minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
-    seconds: Math.floor((distance % (1000 * 60)) / 1000)
+    hours: String(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2,'0'),
+    minutes: String(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).padStart(2,'0'),
+    seconds: String(Math.floor((distance % (1000 * 60)) / 1000)).padStart(2, '0')
   }
 }
 
